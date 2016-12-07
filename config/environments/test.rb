d6b801c6982c36d9f158bb8c762a64a49c3a4d60
annikoff <annikoff@ya.ru>
2016-12-07 22:55:20 +0300

    add input field, script, stylesheet, spec for flickr search

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -20,6 +20,8 @@ Rails.application.configure do
     'Cache-Control' => 'public, max-age=3600'
   }
 
+  config.assets.check_precompiled_asset = false
+
   # Show full error reports and disable caching.
   config.consider_all_requests_local = true
   config.action_controller.perform_caching = false
