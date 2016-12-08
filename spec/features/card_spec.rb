b6d68a49794705657b223fa48c20e1ba161e7018
annikoff <annikoff@ya.ru>
2016-12-08 21:03:28 +0300

    add flickr sercets, update card spec

--- spec/features/card_spec.rb
+++ spec/features/card_spec.rb
@@ -14,7 +14,13 @@ describe 'when user creates card', type: :feature, js: true do
 
   it 'clicks to download link' do
     click_link I18n.t('global.actions.download_from_flickr')
-    expect(page).to have_selector 'input[name="flickr_search"]'
+    expect(page).to have_field 'flickr_search'
     expect(page).to have_button I18n.t('global.actions.find_on_flickr')
+  end
+
+  it 'finds Flickr images by search query' do
+    click_link I18n.t('global.actions.download_from_flickr')
+    fill_in 'flickr_search', with: 'cats'
+    click_button I18n.t('global.actions.find_on_flickr')
   end
 end
