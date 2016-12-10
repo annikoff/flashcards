895a64f3ae7018b52a7a28f1e0504106cad9ae91
annikoff <annikoff@ya.ru>
2016-12-10 15:38:37 +0300

    saving card images from flickr url, specs

--- spec/features/card_spec.rb
+++ spec/features/card_spec.rb
@@ -1,10 +1,11 @@
 # frozen_string_literal: true
 require 'rails_helper'
 
-describe 'when user creates card', type: :feature, js: true do
+describe 'when user creates card', js: true do
   before do
-    create(:user_with_one_block_without_cards)
-    visit new_card_path
+    user = create(:user_with_one_block_and_one_card)
+    @card = user.cards.first
+    visit edit_card_path id: @card.id
     login('test@test.com', '12345', 'Войти')
   end
 
@@ -18,11 +19,15 @@ describe 'when user creates card', type: :feature, js: true do
     expect(page).to have_button I18n.t('global.actions.find_on_flickr')
   end
 
-  it 'finds Flickr images by search query' do
+  it 'finds Flickr images by search query, selects any image and save card' do
     click_link I18n.t('global.actions.download_from_flickr')
     fill_in 'flickr_search', with: 'cats'
     click_button I18n.t('global.actions.find_on_flickr')
     wait_for_ajax
     expect(page).to have_css('.thumbnail', count: 10)
+    page.all(:css, '.thumbnail')[rand(0..9)].click
+    click_button I18n.t('global.actions.save')
+    visit edit_card_path id: @card.id
+    expect(page).to have_css('p.thumbnail img', count: 1)
   end
 end
--- spec/requests/external/flickr_spec.rb
+++ spec/requests/external/flickr_spec.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 require 'rails_helper'
 
-describe External::FlickrController, type: :request do
+describe 'External::Flickr' do
   describe 'photos_search' do
     it 'returns an array of found photos' do
       get photos_search_flickr_index_path,
